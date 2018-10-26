0cb5f5a13e3593c2b321a875c79723a60b498a51
anmolchawla <anmolcha@usc.edu>
2018-10-26 12:08:12 -0700

    Final Git commit
    All changes working

--- shapes.js
+++ shapes.js
@@ -1,10 +1,58 @@
+function values(id){
+	var hold = [];
+	if (id == 76){
+		hold=["Brazil",13.828]
+		return hold;
+	}
+	else if(id == 356){
+		hold=["India",66.465]
+		return hold;
+	}
+	else if(id == 380){
+		hold=["Italy",30.723]
+		return hold;
+	}
+	else if(id == 156){
+		hold=["China",42.097]
+		return hold;
+	}
+	else if(id == 643){
+		hold=["Russia",25.797]
+		return hold;
+	}
+	else if(id == 724){
+		hold=["Spain",19.976]
+		return hold;
+	}
+	else if(id == 752){
+		hold=["Switzerland",25.923]
+		return hold;
+	}
+	else if(id == 840){
+		hold=["U.S",18.04]
+		return hold;
+	}
+	else if(id == 250){
+		hold=["France",20.022]
+		return hold;
+	}
+	else if(id == 566){
+		hold=['Nigeria',50.597]
+		return hold;
+	}
+	else{
+		hold=[0,0]
+		return hold;
+	}
 
+}
 var maps = function(files){
+	 
 	 var data = files[0];
 	 var countries_pop = files[1];
 
 	var margin = {top: 50,left: 50, right:50,bottom: 50},
-				 height = 400 - margin.top - margin.bottom,
+				 height = 600 - margin.top - margin.bottom,
 				 width = 800 - margin.left - margin.right;
 
 
@@ -24,127 +72,92 @@ var maps = function(files){
 
 	var projection = d3.geoMercator()
 					   .translate([width/2,height/2])
-					   .scale(110)
+					   .scale(120)
 	
 
-	var path = d3.geoPath()
-	             .projection(projection)
+	var path = d3.geoPath().projection(projection)
 	
 
 	svg.selectAll(".country")
 	   .data(countries)
 	   .enter()
 	   .append("path")
+	   .attr("stroke-width", "1")
+	   .attr("stroke","black")
 	   .attr("class","country")
+	   .attr("fill","#BC7920")
 	   .attr("d", path);
 
-	//console.log(countries_pop);
-
+       
 	svg.selectAll(".city_circles")
-	   .data(countries_pop)
+	   .data(countries)
 	   .enter()
 	   .append("circle")
-	   .attr("r", function(d){
-		   return d.Rural_Population*0.1;
-	   })
-	   .attr("cx",function(d){
-		   var cords = projection([d.Long, d.lat]);
-		   //console.log(cords);
-		   return cords[0];
-	   })
-	   .attr("cy",function(d){
-		var cords = projection([d.Long, d.lat]);
-		   return cords[1];
-	   });
+	   .attr("fill","red")
+	   .attr("opacity","0.5")
+	   .attr("r",function(d){return (values(d.id)[1]*0.5)} )
+	   .attr('transform', function(d) { return 'translate(' + path.centroid(d) + ')'; })
+	   .on("mouseover",function(d){
+		d3.select(this)
+		.transition()
+		.duration(1000)
+		.attr("opacity","0.7")
+		.attr("r", (values(d.id)[1]));
 
 
-	//    svg.selectAll(".city-labels")
-	// 	  .data(countries_pop)
-	// 	  .enter()
-	// 	  .append("text")
-	// 	  .attr("class","city-labels")
-	// 	  .attr("x",function(d){
-	// 		var cords = projection([d.Long, d.lat]);
-	// 		console.log(cords);
-	// 		return cords[0];
-	// 		})
-	// 		.attr("y",function(d){
-	// 		var cords = projection([d.Long, d.lat]);
-	// 			return cords[1];
-	// 		})
-	// 		.text(function(d){
-	// 			return d.Country_Name;
-	// 		});
- 
+		svg.append("g")
+		.attr("class","names")
+		.append('text')
+		.attr('transform', 'translate(' + path.centroid(d) + ')')
+		.attr('dy', '.35em')
+		.text(values(d.id)[0] + ' : ' + values(d.id)[1] );
+	})
+	.on("mouseout", function(d) {
+
+		d3.select(this)
+		  .transition()
+		  .duration(1000)
+		  .attr("opacity","0.5")
+		  .attr("r", values(d.id)[1]*0.5) 
+
+		  d3.select(".names").remove()
+	});
 
 
 
+
+  svg.append("g")
+     .attr("class","title")
+	 .append("text")
+	 .attr("y",225)
+	 .attr("x",0)
+	 .text("% Rural Population ")
+
+	circles = [0,20,40,60,80,100]
+
+
+	svg.selectAll(".legends")
+	   .data(circles)
+	   .enter()
+	   .append("circle")
+	   .attr("r",function(d){return d*0.5;})
+	   .attr("cx",function(d,i){return 20})
+	   .attr("cy",function(d,i){return 200+(4*(d*0.5))})
+	   .attr("fill","red")
+	   .attr("opacity","0.5")
+
+	   svg.selectAll(".legends")
+	   .data(circles)
+	   .enter()
+	   .append("text")
+	   .attr("x",function(d,i){return 9})
+	   .attr("y",function(d,i){return 205+(4*(d*0.5))})
+	   .text(function(d){return d})
+
+
+
+   
 	  
-			//  var cities=[
-			// 	  {"code":"ZNZ", "city":"ZANZIBAR", "country":"TANZANIA", "lat":-6.13, "lon":39.31},
-			// 	  {"code":"TYO", "city":"TOKYO", "country":"JAPAN", "lat":35.68, "lon":139.76},
-			// 	  {"code":"AKL", "city":"AUCKLAND", "country":"NEW ZEALAND", "lat":-36.85, "lon":174.78},
-			// 	  {"code":"BKK", "city":"BANGKOK", "country":"THAILAND", "lat":13.75, "lon":100.48},
-			// 	  {"code":"DEL", "city":"DELHI", "country":"INDIA", "lat":29.01, "lon":77.38},
-			// 	  {"code":"SIN", "city":"SINGAPORE", "country":"SINGAPOR", "lat":1.36, "lon":103.75},
-			// 	  {"code":"BSB", "city":"BRASILIA", "country":"BRAZIL", "lat":-15.67, "lon":-47.43},
-			// 	  {"code":"RIO", "city":"RIO DE JANEIRO", "country":"BRAZIL", "lat":-22.9, "lon":-43.24},
-			// 	  {"code":"YTO", "city":"TORONTO", "country":"CANADA", "lat":43.64, "lon":-79.4},
-			// 	  {"code":"IPC", "city":"EASTER ISLAND", "country":"CHILE", "lat":-27.11, "lon":-109.36},
-			// 	  {"code":"SEA", "city":"SEATTLE", "country":"USA", "lat":47.61, "lon":-122.33},
-			// 	  {"code":"LON", "city":"LONDON", "country":"UK", "lat":51.5072, "lon":-0.1275}
-			// 	]; 
-				
-			
-				
-				
-
-					
-			   
-				
-			// 	var path = d3.geoPath().projection(projection);
-				
-			// 	var g = svg.append("g");
-
-               
-				
-			
-				
-			// 	var world = g.selectAll("path")
-			// 					.data(data.objects.countries.geometries)
-			// 					.enter()
-			// 					.append("path")
-			// 					.attr("d", path)
-				
-			// 	console.log("here");
-			// 	// // add city location circles
-			// 	var locations = g.selectAll("circle")
-			// 					   .data(cities)
-			// 					   .enter()
-			// 					   .append("circle")
-			// 					   .attr("cx", function(d) {return projection([d.lon, d.lat])[0];})
-			// 					   .attr("cy", function(d) {return projection([d.lon, d.lat])[1];})
-			// 					   .attr("r", 5)
-			// 					   .style("fill", "red")
-			// 					   .style("opacity", 0.6);
-					
-		
-				  
-				
-				
-			// 	// //zoom and pan functionality
-			// 	// var zoom = d3.behavior.zoom()
-			// 	// 	.on("zoom",function() {
-			// 	// 		g.attr("transform","translate("+ 
-			// 	// 			d3.event.translate.join(",")+")scale("+d3.event.scale+")");
-			// 	// 		g.selectAll("path")  
-			// 	// 			.attr("d", path.projection(projection)); 
-			// 	//   });
-			// 	// svg.call(zoom);
-
-
-
-
 
 return ;
 }
@@ -152,9 +165,18 @@ return ;
 var maps1 = function(files){
 	var data = files[0];
 	var countries_pop = files[1];
+	var format = d3.format("")
+	var color = d3.scaleQuantize()
+				  .domain([1, 10])
+				  .range(d3.schemeBlues[9])
+	var x = d3.scaleLinear()
+			  .domain(d3.extent(color.domain()))
+			  .rangeRound([600, 860]);
+
+
 
    var margin = {top: 50,left: 50, right:50,bottom: 50},
-				height = 400 - margin.top - margin.bottom,
+				height = 600 - margin.top - margin.bottom,
 				width = 800 - margin.left - margin.right;
 
 
@@ -169,8 +191,8 @@ var maps1 = function(files){
 	   
 	
    var countries = topojson.feature(data, data.objects.countries).features
-
-   console.log(countries);
+   //console.log("below for world");
+   //console.log(countries);
 
    var projection = d3.geoMercator()
 					  .translate([width/2,height/2])
@@ -180,42 +202,40 @@ var maps1 = function(files){
    var path = d3.geoPath()
 				.projection(projection)
    
+//console.log(countries);
 
    svg.selectAll(".country")
 	  .data(countries)
 	  .enter()
 	  .append("path")
 	  .attr("class","country")
-	  //.attr("fill", d => console.log(color[0]))
-	  .attr("d", path);
-	//   .attr("fill", d => color(data.get(d.id)))
-	//   .append("title")
-	//   .text(d => format(data.get(d.id)));
+	  .attr("d", path)
+	  .attr("stroke-width", "1")
+	  .attr("stroke","black")
+	  .attr("fill","white")
 
 
-	// svg.append("g")
-	//   .selectAll("path")
-	//   .data(topojson.feature(us, us.objects.counties).features)
-	//   .enter().append("path")
-	// 	.attr("fill", d => color(data.get(d.id)))
-	// 	.attr("d", path)
-	//   .append("title")
-	// 	.text(d => format(data.get(d.id)));
-  
+	  svg.selectAll(".country")
+		  .data(countries)
+		  .attr("fill",function(d,i){return color(values(d.id)[1]*0.1)     })
+	       .on("mouseover",function(d){
+			   if (values(d.id)[1] != 0){
+				svg.append("g")
+				.attr("class","names")
+				.append('text')
+				.style("fill","red")
+				.style("font-weight", "bold")
+				.attr('transform', 'translate(' + path.centroid(d) + ')')
+				.attr('dy', '.35em')
+				.text(values(d.id)[0] + ' : ' + values(d.id)[1] );
+			   }
+			})
+			.on("mouseout", function(d) {
+					d3.select(".names").remove()
+			});
+		
 
-
-
-   var format = d3.format("")
-   var color = d3.scaleQuantize()
-                 .domain([1, 10])
-                 .range(d3.schemeBlues[9])
-   var x = d3.scaleLinear()
-             .domain(d3.extent(color.domain()))
-             .rangeRound([600, 860]);
-
- 
-
-   var g = svg.append("g").attr("transform", "translate(-650,300)");
+var g = svg.append("g").attr("transform", "translate(-650,300)");
 
   g.selectAll("rect")
     .data(color.range().map(d => color.invertExtent(d)))
@@ -252,51 +272,101 @@ return ;
 
 
 var maps2 = function(files){
-	var data = files[2];
-
-   var margin = {top: 50,left: 50, right:50,bottom: 50},
-				height = 400 - margin.top - margin.bottom,
-				width = 800 - margin.left - margin.right;
-
-
-	   
-
-   var svg = d3.select("#canva2")
-			   .append("svg")
-			   .attr("height",height + margin.top + margin.bottom)
-			   .attr("width",width + margin.left + margin.right)
-			   .append('g')
-			   .attr("transform","translate(" + margin.left + "," + margin.top + ")");
-
-			   
-
 	
-   var countries = topojson.feature(data, data.objects['City Boundaries']).features
+	var maps = files[3];
+	var data = files[2];
+	//console.log(maps);
 
-  console.log(countries);
 
-   var projection = d3.geoMercator()
-					  .translate([width/2,height/2])
-					  .scale(150);
-   
+	const width = 800;
+    const height = 600;
+    const svg = d3.select('#canva2').append('svg')
+      .attr('height', width+00)
+	  .attr('width', height+100);
+	  
+	  
 
-   var path = d3.geoPath()
-				.projection(projection)
-   
 
-   svg.selectAll(".county")
-	  .data(countries)
-	  .enter()
-	  .append("path")
-	  .attr("class","county")
-	  .attr("d", path);
+      // Create a unit projection.
+      const laProjection = d3.geoAlbers().scale(1).translate([0, 0]);
+
+      // Create a path generator                              
+      var path = d3.geoPath().projection(laProjection);
+
+      // Compute the bounds of a feature of interest, 
+      // then derive scale & translate.
+      const laBounds = path.bounds(maps);
+      const laScale = 0.95 / Math.max(
+                (laBounds[1][0] - laBounds[0][0]) / width,
+                (laBounds[1][1] - laBounds[0][1]) / height
+              );
+      const laTranslate = [
+        (width - laScale * (laBounds[1][0] + laBounds[0][0])) / 2, 
+        (height - laScale * (laBounds[1][1] + laBounds[0][1])) / 2
+      ];
+
+      // Update the projection to use computed scale & translate.
+      //console.log(laBounds);
+      //console.log(laScale, laTranslate)
+      laProjection.scale(laScale)
+                  .translate(laTranslate);
+
+      svg.append('path')
+          .datum(maps)
+		  .attr('d', path)
+		  .attr("stroke-width","1")
+		  .attr("stroke","black")
+		  .attr("fill","#C6DBEF");
+	
+		  //console.log("park",data)
+var parks = topojson.feature(data, data.objects.Parks_and_Gardens).features
+
+
+
+// d3.map(parks, function(d) {
+// 	// show all the properties of each feature:
+// 	d3.map(d3.keys(d.properties), function(name) {
+// 		console.log(name + " : " + d.properties[name]);
+// 	})
+// });
+
+console.log("parksss",parks[0].properties.name) 
+
+ svg.selectAll(".parks")
+    .data(parks)
+	.enter()
+	.append("path")
+	.attr("stroke-width", "1")
+	.attr("stroke","red")
+	.attr("class","parks")
+	.attr("fill","#BC7920")
+	.attr("d", path)
+	.on("mouseover",function(d){
+		console.log(d.properties.name)
+	  svg.append("g")
+		 .attr("class","names")
+		 .append('text')
+		 .style("fill","black")
+		 .style("font-weight", "bold")
+		 .attr('transform', 'translate(' + path.centroid(d) + ')')
+		 .attr('dy', '.35em')
+		 .text(d.properties.name);
+
+
+	 })
+	 .on("mouseout", function(d) {
+			d3.select(".names").remove()
+			d3.select(".bg").remove()
+	 });;
+
 return ;
 }
 
 Promise.all([
 	d3.json("world.topojson"),
 	d3.csv("world.csv"),
-	d3.json("LA.topojson")
+	d3.json("Parks_and_Gardens.topojson"),
+	d3.json("la.json"),
 ]).then(function (files) { 
   //console.log(data);
   maps(files);
